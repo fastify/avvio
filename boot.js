953642b1a5acb15b8924da0a745f376e56c242d2
Matteo Collina <hello@matteocollina.com>
2016-08-13 23:24:52 +0200

    Support reentrant plugin loading.

--- boot.js
+++ boot.js
@@ -16,8 +16,15 @@ function Boot (server, done) {
 
   server = server || this
 
-  this._queue = fastq(server, function (toLoad, cb) {
-    process.nextTick(toLoad.plugin, server, toLoad.opts, cb)
+  this._queue = fastq(server, (toLoad, cb) => {
+    const func = toLoad.plugin
+    func(server, toLoad.opts, (err) => {
+      // schedule all tasks left in the batch
+      clear(this)
+
+      // always delay the next task
+      process.nextTick(cb, err)
+    })
   }, 1)
 
   if (done) {
@@ -28,6 +35,7 @@ function Boot (server, done) {
     this.emit('start')
   }
 
+  this._batch = null
   this.use(nooplugin)
 }
 
@@ -35,10 +43,39 @@ inherits(Boot, EE)
 
 Boot.prototype.use = function (plugin, opts) {
   opts = opts || {}
-  this._queue.push({
+
+  const obj = {
     plugin,
     opts
-  })
+  }
+
+  if (this._batch) {
+    this._batch.push(obj)
+  } else {
+    this._batch = [obj]
+    // add all the current batch in the next tick
+    process.nextTick(clear, this)
+  }
+}
+
+// add all element in the batch at the top of the
+// queue, in the order that they are called with use()
+function clear (boot) {
+  // needed if the plugin loads sync
+  if (!boot._batch) {
+    return
+  }
+
+  // reverse the batch
+  const batch = boot._batch.reverse()
+  boot._batch = null
+
+  // we need to pause, otherwise one of the jobs might be triggered
+  // and we will trigger the wrong one, because we are adding them
+  // at the top
+  boot._queue.pause()
+  batch.forEach((obj) => boot._queue.unshift(obj))
+  boot._queue.resume()
 }
 
 function nooplugin (s, o, done) {
