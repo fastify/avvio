2ce76cc9571a0c56bd7a823b05b3d3cd48ed5bc9
Manuel Spigolon <behemoth89@gmail.com>
2019-08-05 22:15:38 +0200

    add use inside after

--- test/pretty-print.test.js
+++ test/pretty-print.test.js
@@ -4,13 +4,13 @@ const test = require('tap').test
 const boot = require('..')
 
 test('pretty print', t => {
-  t.plan(18)
+  t.plan(19)
 
   const app = boot()
   app
     .use(first)
     .use(duplicate, { count: 3 })
-    .use(second).after(after).after(after)
+    .use(second).after(afterUse).after(after)
     .use(duplicate, { count: 2 })
     .use(third).after(after)
     .use(duplicate, { count: 1 })
@@ -22,7 +22,8 @@ test('pretty print', t => {
     /│ {3}└─┬ duplicate \d+ ms/,
     /│ {5}└── duplicate \d+ ms/,
     /├── second \d+ ms/,
-    /├── bound _after \d+ ms/,
+    /├─┬ bound _after \d+ ms/,
+    /│ └── afterInsider \d+ ms/,
     /├── bound _after \d+ ms/,
     /├─┬ duplicate \d+ ms/,
     /│ └─┬ duplicate \d+ ms/,
@@ -55,6 +56,14 @@ test('pretty print', t => {
   }
   function after (err, cb) {
     cb(err)
+  }
+  function afterUse (err, cb) {
+    app.use(afterInsider)
+    cb(err)
+  }
+
+  function afterInsider (s, opts, done) {
+    done()
   }
 
   function duplicate (instance, opts, cb) {
