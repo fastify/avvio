e80ccd39783903c231e96642026dc4488cc51407
Matteo Collina <hello@matteocollina.com>
2019-12-16 01:27:18 +0100

    Remove esm module in tests

--- test/esm.test.js
+++ test/esm.test.js
@@ -7,7 +7,7 @@ if (semver.lt(process.versions.node, '13.3.0')) {
   tap.pass('Skip because Node version <= 13.3.0')
   tap.end()
 } else {
-  import('./esm.mjs').catch((err) => {
+  global.import('./esm.mjs').catch((err) => {
     process.nextTick(() => {
       throw err
     })
