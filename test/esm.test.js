f352f45756a15ca35fef088e2418b055fe59bbb8
Matteo Collina <hello@matteocollina.com>
2019-12-16 01:39:39 +0100

    Fixed test

--- test/esm.test.js
+++ test/esm.test.js
@@ -7,7 +7,9 @@ if (semver.lt(process.versions.node, '13.3.0')) {
   tap.pass('Skip because Node version <= 13.3.0')
   tap.end()
 } else {
-  global.import('./esm.mjs').catch((err) => {
+  // Import is not allowed syntax in Node 8
+  // eslint-disable-next-line
+  new Function('module', 'return import(module)')('./esm.mjs').catch((err) => {
     process.nextTick(() => {
       throw err
     })
