2ecc96c523c16d169ecc925cc3dc76cc05bbcaad
David Mark Clements <huperekchuno@googlemail.com>
2020-01-09 15:48:31 +0100

    fix version check

--- test/async-await.test.js
+++ test/async-await.test.js
@@ -1,8 +1,7 @@
 'use strict'
 
 const tap = require('tap')
-
-if (Number(process.versions.node[0]) >= 8) {
+if (Number(process.versions.node.split('.')[0]) >= 8) {
   require('./async-await')
 } else {
   tap.pass('Skip because Node version < 8')
