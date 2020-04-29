2d21cc005adc2b37bf2cf32863fd65eecadcef34
Matteo Collina <hello@matteocollina.com>
2020-04-29 11:15:07 +0200

    Fix chainable API

--- test/chainable.test.js
+++ test/chainable.test.js
@@ -69,3 +69,29 @@ test('chainable automatically binded', (t) => {
     t.end()
   })
 })
+
+test('chainable standalone with server', (t) => {
+  t.plan(6)
+
+  const server = {}
+  boot(server, {
+    expose: {
+      use: 'register'
+    }
+  })
+
+  const readyResult = server.register(function (ctx, opts, done) {
+    t.pass('1st plugin')
+    done()
+  }).after(function (err, done) {
+    t.error(err)
+    t.pass('2nd after')
+    done()
+  }).register(function (ctx, opts, done) {
+    t.pass('3rd plugin')
+    done()
+  }).ready(function () {
+    t.pass('we are ready')
+  })
+  t.equal(readyResult, undefined)
+})
