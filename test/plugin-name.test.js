0996aa68ca0ee7df2c99e12ad9980e1fef30c15f
Harry Brundage <harry.brundage@gmail.com>
2021-11-07 06:17:59 -0500

    Use fastify plugin metadata or an explicit passed in name option for a plugin's name (#162)

---
+++ test/plugin-name.test.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const t = require('tap')
+const test = t.test
+const boot = require('..')
+
+test('plugins get a name from the plugin metadata if it is set', async (t) => {
+  t.plan(1)
+  const app = boot()
+
+  const func = (app, opts, next) => next()
+  func[Symbol.for('plugin-meta')] = { name: 'a-test-plugin' }
+  app.use(func)
+  await app.ready()
+
+  t.match(app.toJSON(), {
+    label: 'bound root',
+    nodes: [
+      { label: 'a-test-plugin' }
+    ]
+  })
+})
+
+test('plugins get a name from the options if theres no metadata', async (t) => {
+  t.plan(1)
+  const app = boot()
+
+  function testPlugin (app, opts, next) { next() }
+  app.use(testPlugin, { name: 'test registration options name' })
+  await app.ready()
+
+  t.match(app.toJSON(), {
+    label: 'bound root',
+    nodes: [
+      { label: 'test registration options name' }
+    ]
+  })
+})
+
+test('plugins get a name from the function name if theres no name in the options and no metadata', async (t) => {
+  t.plan(1)
+  const app = boot()
+
+  function testPlugin (app, opts, next) { next() }
+  app.use(testPlugin)
+  await app.ready()
+
+  t.match(app.toJSON(), {
+    label: 'bound root',
+    nodes: [
+      { label: 'testPlugin' }
+    ]
+  })
+})
+
+test('plugins get a name from the function source if theres no other option', async (t) => {
+  t.plan(1)
+  const app = boot()
+
+  app.use((app, opts, next) => next())
+  await app.ready()
+
+  t.match(app.toJSON(), {
+    label: 'bound root',
+    nodes: [
+      { label: '(app, opts, next) => next()' }
+    ]
+  })
+})
