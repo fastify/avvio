0fffce567c95f73abafb3bb6a8e6ffd37d72a0bb
SindreXie <xtx1130@gmail.com>
2022-03-21 16:06:43 +0800

    test: change case for esm test

--- test/esm.test.js
+++ test/esm.test.js
@@ -4,16 +4,12 @@ const { test } = require('tap')
 const semver = require('semver')
 
 test('support import', { skip: semver.lt(process.versions.node, '13.3.0') }, (t) => {
-    t.pass('Skip because Node version <= 13.3.0')
+  import('./esm.mjs').then(() => {
+    t.pass('esm is supported')
     t.end()
-  } else {
-    import('./esm.mjs').then(() => {
-      t.pass('esm is supported')
-      t.end()
-    }).catch((err) => {
-      process.nextTick(() => {
-        throw err
-      })
+  }).catch((err) => {
+    process.nextTick(() => {
+      throw err
     })
-  }
+  })
 })
