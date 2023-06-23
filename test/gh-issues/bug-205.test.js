d17fbfd29fdf24a8e1a70a51d80e9ae12df78bdc
uzlopak <aras.abbasi@googlemail.com>
2023-06-23 20:27:01 +0200

    add test as issue #205 is also solved.

---
+++ test/gh-issues/bug-205.test.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const { test } = require('tap')
+const boot = require('../..')
+
+test('should print the time tree', (t) => {
+  t.plan(2)
+  const app = boot()
+
+  app.use(function first (instance, opts, cb) {
+    const out = instance.prettyPrint().split('\n')
+    t.equal(out[0], 'bound root undefined ms')
+    t.equal(out[1], '└── first undefined ms')
+    cb()
+  })
+})
