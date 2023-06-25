12cf9116d87ca640ea0c452c0b0804257dc9c8dc
Uzlopak <aras.abbasi@googlemail.com>
2023-06-25 12:46:23 +0200

    extract create-promise (#218)
    * extract create-promise
    
    * Update lib/create-promise.js
    
    Co-authored-by: Matteo Collina <hello@matteocollina.com>
    
    ---------
    
    Co-authored-by: Matteo Collina <hello@matteocollina.com>

--- plugin.js
+++ plugin.js
@@ -4,19 +4,9 @@ const fastq = require('fastq')
 const EE = require('events').EventEmitter
 const inherits = require('util').inherits
 const { debug } = require('./lib/debug')
+const { createPromise } = require('./lib/create-promise')
 const { AVV_ERR_READY_TIMEOUT } = require('./lib/errors')
 const { getPluginName } = require('./lib/get-plugin-name')
-
-function promise () {
-  const obj = {}
-
-  obj.promise = new Promise((resolve, reject) => {
-    obj.resolve = resolve
-    obj.reject = reject
-  })
-
-  return obj
-}
 
 function Plugin (parent, func, options, isAfter, timeout) {
   this.started = false
@@ -144,7 +134,7 @@ Plugin.prototype.loadedSoFar = function () {
   let res
 
   if (!this._promise) {
-    this._promise = promise()
+    this._promise = createPromise()
     res = this._promise.promise
 
     if (!this.server) {
