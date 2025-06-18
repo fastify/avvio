adddffd9f1fe4492516529b7892db47100b3b2d2
Tony133 <a.tripodi133@gmail.com>
2025-06-18 22:37:18 +0200

    test: migrated tests to node:test

--- test/lib/validate-plugin.test.js
+++ test/lib/validate-plugin.test.js
@@ -1,19 +1,19 @@
 'use strict'
 
-const { test } = require('tap')
+const { test } = require('node:test')
 const { validatePlugin } = require('../../lib/validate-plugin')
 const { AVV_ERR_PLUGIN_NOT_VALID } = require('../../lib/errors')
 
 test('validatePlugin', (t) => {
   t.plan(8)
 
-  t.throws(() => validatePlugin(1), new AVV_ERR_PLUGIN_NOT_VALID('number'))
-  t.throws(() => validatePlugin('function'), new AVV_ERR_PLUGIN_NOT_VALID('string'))
-  t.throws(() => validatePlugin({}), new AVV_ERR_PLUGIN_NOT_VALID('object'))
-  t.throws(() => validatePlugin([]), new AVV_ERR_PLUGIN_NOT_VALID('array'))
-  t.throws(() => validatePlugin(null), new AVV_ERR_PLUGIN_NOT_VALID('null'))
+  t.assert.throws(() => validatePlugin(1), new AVV_ERR_PLUGIN_NOT_VALID('number'))
+  t.assert.throws(() => validatePlugin('function'), new AVV_ERR_PLUGIN_NOT_VALID('string'))
+  t.assert.throws(() => validatePlugin({}), new AVV_ERR_PLUGIN_NOT_VALID('object'))
+  t.assert.throws(() => validatePlugin([]), new AVV_ERR_PLUGIN_NOT_VALID('array'))
+  t.assert.throws(() => validatePlugin(null), new AVV_ERR_PLUGIN_NOT_VALID('null'))
 
-  t.doesNotThrow(() => validatePlugin(function () {}))
-  t.doesNotThrow(() => validatePlugin(new Promise((resolve) => resolve)))
-  t.doesNotThrow(() => validatePlugin(Promise.resolve()))
+  t.assert.doesNotThrow(() => validatePlugin(function () {}))
+  t.assert.doesNotThrow(() => validatePlugin(new Promise((resolve) => resolve)))
+  t.assert.doesNotThrow(() => validatePlugin(Promise.resolve()))
 })
